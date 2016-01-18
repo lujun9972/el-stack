ce2d897c80cf6ba9bbebdb46feddc9e69f90ebfc
lujun9972 <lujun9972@gmail.com>
2016-01-19 07:53:55 +0800

    add stack-resize

--- stack-vector.el
+++ stack-vector.el
@@ -6,27 +6,27 @@
   ((content :initform (vector) :initarg :content :protection :protected)
    (top :initform -1)))
 
-(defun stack-vector-create (capacity)
+(defmethod stack-vector-create (capacity)
   "Create a stack"
   (make-instance 'stack-vector :content (make-vector capacity nil)))
 
-(defun stack-length ((stack-object stack-vector))
+(defmethod stack-length ((stack-object stack-vector))
   (+ 1 (oref stack-object top)))
 
-(defun stack-clear ((stack-object stack-vector))
+(defmethod stack-clear ((stack-object stack-vector))
   (dotimes (n (stack-length stack-object))
     (setf (aref stack-object n) nil)))
 
-(defun stack-empty-p ((stack-object stack-vector))
+(defmethod stack-empty-p ((stack-object stack-vector))
   (= 0 (stack-length stack-object)))
 
-(defun stack-top ((stack-object stack-vector))
+(defmethod stack-top ((stack-object stack-vector))
   (when (stack-empty-p stack-object)
     (error "stack is empty"))
   (aref (stack-content stack-object)
         (oref stack-object top)))
 
-(defun stack-pop ((stack-object stack-vector))
+(defmethod stack-pop ((stack-object stack-vector))
   (when (stack-empty-p stack-object)
     (error "stack is empty"))
   (let ((element (stack-top stack-object))
@@ -36,14 +36,25 @@
     (cl-incf (oref stack-object top) -1)
     element))
 
-(defun stack-full-p ((stack-object stack-vector))
+(defmethod stack-full-p ((stack-object stack-vector))
   "Wether the stack is full"
   (= (oref stack-object top) (- (length (stack-content stack-object)) -1)))
 
-(defun stack-push ((stack-object stack-vector) element)
+(defmethod stack-push ((stack-object stack-vector) element)
   (when (stack-full-p stack-object)
     (error "stack is full"))
   (cl-incf (oref stack-object top))
   (setf (aref (stack-content stack-object)
               (oref stack-object top))
         element))
+
+(defmethod stack-resize ((stack-object stack-vector) &optional new-capacity)
+  "resize the stack"
+  (let* ((old-capacity (length (stack-content stack)))
+         (new-capacity (or new-capacity
+                           (* 2 old-capacity)))
+         (new-content (make-vector new-capacity nil)))
+    (dotimes (n (stack-length stack-object))
+      (setf (aref new-capacity n)
+            (aref (stack-content stack-object) n)))
+    stack-object))
